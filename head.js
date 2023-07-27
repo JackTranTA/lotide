ee38464c7c04569e315a1211daa37655e8ed328c
Jack Tran <bluestar113311@gmail.com>
2023-07-26 17:20:51 -0700

    Added head.js

---
+++ head.js
@@ -0,0 +1,14 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🔥🔥🔥Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`⛔⛔⛔Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const head = function(array) {
+    return array[0]; //or array.shift();
+};
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
