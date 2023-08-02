d37181b78d4c0ec73436dbb29f66839712e49616
Jack Tran <bluestar113311@gmail.com>
2023-08-02 08:32:08 -0700

    Refactored head.js

--- head.js
+++ head.js
@@ -7,8 +7,12 @@ const assertEqual = function(actual, expected) {
 };
 
 const head = function(array) {
-    return array[0]; //or array.shift();
+  if (array.length > 0)
+    return array[0];
+  else
+    return undefined;
 };
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+// assertEqual(head([5,6,7]), 5);
+// assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+module.exports = head;
