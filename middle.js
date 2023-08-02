9716004d4f02951e403a98201f134a51cfe24202
Jack Tran <bluestar113311@gmail.com>
2023-08-02 09:04:07 -0700

    Refactored assertmiddle.js and added middleTest.js

--- middle.js
+++ middle.js
@@ -1,46 +1,17 @@
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  } else {
-    for(let i = 0; i > arr1.length; i++) {
-      if(arr1[i] !== arr2[i]) {
-        return false;
-      }
-    }
-    return true;
-  }
-};
-
-const assertArraysEqual = function(arr1, arr2) {
-  if (eqArrays(arr1, arr2)) {
-    console.log(`🔥🔥🔥Assertion Passed: [${arr1}] === [${arr2}]`);
-  } else {
-    console.log(`⛔⛔⛔Assertion Failed: [${arr1}] !== [${arr2}]`);
-  }
-};
-
 const middle = function(arr) {
   const middle = [];
   if (arr.length < 3) {
-    console.log(middle);
     return middle;
   }
 
   if (arr.length % 2 === 1) {
     middle.push(arr[(arr.length - 1) / 2]);
-    console.log(middle);
     return middle;
   }
 
   middle.push(arr[arr.length / 2 - 1]);
   middle.push(arr[arr.length / 2]);
-  console.log(middle);
   return middle;
 };
 
-middle([1]) // => []
-middle([1, 2]) // => []
-middle([1, 2, 3]) // => [2]
-middle([1, 2, 3, 4, 5]) // => [3]
-middle([1, 2, 3, 4]) // => [2, 3]
-middle([1, 2, 3, 4, 5, 6]) // => [3, 4]
+module.exports = middle;
